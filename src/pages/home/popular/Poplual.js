f48c2aa6b516188d6d0cead3101ba1455618675b
hitvaghani26 <vaghanihit123@gmail.com>
2023-11-02 22:57:28 +0530

    change in endpoints in Populal.js

--- src/pages/home/popular/Poplual.js
+++ src/pages/home/popular/Poplual.js
@@ -21,7 +21,7 @@ const Popular = () => {
         </span>
           <SwitchTabs data ={["Movies", "Tv Shows"]} onTabChange={onTabChange}></SwitchTabs>
       </ContentWrapper>
-      <Carousal data={data?.results} loading={loading} endPoint={endPoint} />
+      <Carousal data={data?.results} loading={loading} endpoint={endPoint} />
     </div>
   )
 }
